9920825ad3325183ec3d8804c7aa254ef40bab4a
Jakaria <jakariasami@gmail.com>
2024-05-01 11:24:08 +0600

    socket setup

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def home():
     name = request.form.get("name")
     code = request.form.get("code")
     join = request.form.get("join", False)
-    create = request.form.get("cerate", False)
+    create = request.form.get("create", False)
     
     if not name:
       return render_template("home.html", error="Enter a name", code=code, name=name)
@@ -38,7 +38,7 @@ def home():
       return render_template("home.html", error="Enter a room code", code=code, name=name)
     
     room = code
-    if create:
+    if create != False:
       room = generate_unique_code(4)
       rooms[room] = {"members": 0, "messages":[]}
     elif code not in rooms:
@@ -50,10 +50,46 @@ def home():
 
   return render_template("home.html")
 
+
 @app.route("/room")
 def room():
+  room = session.get("room")
+  if room is None or session.get("name") is None or room not in rooms:
+    return redirect(url_for("home"))
+  
   return render_template("room.html")
 
+
+@socketio.on("connect")
+def connect(auth):
+  room = session.get("room")
+  name = session.get("name")
+  if not room or not name:
+    return
+  if room not in rooms:
+    leave_room(room)
+    return
+  
+  join_room(room)
+  send({"name": name, "message": "joined the room"}, to=room)
+  rooms[room]["members"] += 1
+  print(f"{name} joined room {room}")
+
+
+@socketio.on("disconnect")
+def disconnect():
+  room = session.get("room")
+  name = session.get("name")
+  leave_room(room)
+
+  if room in rooms: 
+    rooms[room]["members"] -= 1
+    if rooms[room]["members"] <= 0:
+      del rooms[room]
+
+  send({"name": name, "message": "has left"}, to=room)
+  print(f"{name} has left the {room}")
+  
 
 if __name__ == "__main__":
   socketio.run(app, debug=True)
